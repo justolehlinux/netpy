fe53ce837bc3f2e7706d7d676f6358c238a9a237
justolehlinux <114715041+justolehlinux@users.noreply.github.com>
2024-01-20 02:15:46 +0100

    1class

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import netaddr 
 
-ip = IPAddress('192.168.0.1')
-ip.version
+class host:
+    def __init__(self, num, ip, dev): 
+        self.IP = netaddr.IPAddress(ip)
+        self.MAC = netaddr.EUI('00-1B-77-49-54-FD')
+        if self.dev is not None:
+            self.dev = dev 
+        else: self.dev = "HOST"
+        # self.NAME = self.dev + str(num)
 
+    def name(self):
+        return self.NAME
 
+    host1 = host(22,'192.168.0.2','router')
+    print (host1.name())
